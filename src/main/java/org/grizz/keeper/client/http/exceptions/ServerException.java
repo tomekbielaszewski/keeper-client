21dc05998e54d7a533a302fd8c9198b6e6752b5b
Grizwold <tomekbielaszewski@gmail.com>
2017-08-05 13:34:38 +0200

    Wrapping keeper entry values in Map to prevent posting primitives which resulted with error on keeper

--- src/main/java/org/grizz/keeper/client/http/exceptions/ServerException.java
+++ src/main/java/org/grizz/keeper/client/http/exceptions/ServerException.java
@@ -3,13 +3,15 @@ package org.grizz.keeper.client.http.exceptions;
 import lombok.ToString;
 import org.grizz.keeper.client.model.KeeperEntry;
 
+import static org.grizz.keeper.client.model.Value.of;
+
 @ToString(callSuper = true)
 public class ServerException extends KeeperApiException {
     public ServerException() {
         super(
                 KeeperEntry.builder()
                         .key("ERROR")
-                        .value("Unknown internal server error. Check the logs or notify The Admin.")
+                        .value(of("description", "Unknown internal server error. Check the logs or notify The Admin."))
                         .owner("https://www.wykop.pl/ludzie/Grizwold/")
                         .date(System.currentTimeMillis())
                         .build()
--- src/main/java/org/grizz/keeper/client/model/KeeperEntry.java
+++ src/main/java/org/grizz/keeper/client/model/KeeperEntry.java
@@ -3,12 +3,14 @@ package org.grizz.keeper.client.model;
 import lombok.Builder;
 import lombok.Data;
 
+import java.util.Map;
+
 @Data
 @Builder
 public class KeeperEntry<T> {
     private String id;
     private String key;
-    private T value;
+    private Map<String, T> value;
     private String owner;
     private Long date;
 }
